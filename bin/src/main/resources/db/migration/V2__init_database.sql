0f9eadb46d3924991cf7d6dbbc0fb60457d3ad45
User <User@Lenovo>
2025-03-13 22:00:24 +0100

    rollback of commits

---
+++ bin/src/main/resources/db/migration/V2__init_database.sql
@@ -0,0 +1,25 @@
+CREATE SCHEMA IF NOT EXISTS school;
+
+CREATE TABLE school.GROUPS (
+    group_id SERIAL PRIMARY KEY,
+    group_name VARCHAR(255)
+);
+
+CREATE TABLE school.COURSES (
+    course_id SERIAL PRIMARY KEY,
+    course_name VARCHAR(255),
+    course_description VARCHAR(255)
+);
+
+CREATE TABLE school.STUDENTS (
+    student_id SERIAL PRIMARY KEY,
+    group_id INTEGER REFERENCES school.GROUPS(group_id),
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE School.STUDENTS_COURSES (
+    student_id INTEGER REFERENCES School.STUDENTS(student_id),
+    course_id INTEGER REFERENCES School.COURSES(course_id),
+    PRIMARY KEY (student_id, course_id)
+);
